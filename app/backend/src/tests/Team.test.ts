7b8aba5bec0e4509c7406cd726f1d6bc6b60436e
arthur-debiasi <arthurdebiasi@hotmail.com>
2023-02-28 11:49:44 -0300

    feat: req 04

--- app/backend/src/tests/Team.test.ts
+++ app/backend/src/tests/Team.test.ts
@@ -16,11 +16,21 @@ const { expect } = chai;
 describe('Testes do endpoint /teams:', () => {
   beforeEach(() => {
     sinon.stub(Teams, 'findAll').resolves(teamsListMock as Teams[]);
+    sinon.stub(Teams, 'findByPk').resolves(teamsListMock[0] as Teams);
   });
 
-  it('que tenha response com status 200 e body com a lista de times', async () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('que GET /teams tenha response com status 200 e body com a lista de times', async () => {
     const res = await chai.request(app).get('/teams');
-    expect(res.status).to.be.eq(200);  
+    expect(res.status).to.be.eq(200);
     expect(res.body).to.be.deep.eq(teamsListMock);
+  });
+  it('que GET /teams/:id tenha response com status 200 e o time correspondente ao id', async () => {
+    const res = await chai.request(app).get('teams/1');
+    expect(res.status).to.be.eq(200);
+    expect(res.body).to.be.deep.eq(teamsListMock[0]);
   });
 });
